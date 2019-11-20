34e24fe1e4362424d2001384a5b18f8bc25029a1
PerfilevVA <perfilevva@sibedge.com>
2019-11-20 21:13:02 +0700

    Create base MVVM

---
+++ PEJournal/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEJournal.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{LastName} {FirstName} {MiddleName}";
+            }
+        }
+    }
+}
---
+++ PEJournal/ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using PEJournal.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEJournal.ViewModels
+{
+    public class MainViewModel : Notifier
+    {
+        public ObservableCollection<Student> Students { get; set; }
+
+        private Student selectedStudent;
+        public Student SelectedStudent
+        {
+            get { return selectedStudent; }
+            set
+            {
+                selectedStudent = value;
+                NotifyPropertyChanged("SelectedStudent");
+            }
+        }
+        public MainViewModel()
+        {
+            Students = new ObservableCollection<Student>
+            {
+                new Student { Id = 1, FirstName = "Александр", LastName = "Лужков", MiddleName = "Иванович" },
+                new Student { Id = 2, FirstName = "Андрей", LastName = "Колотаев", MiddleName = "Кириллович" },
+                new Student { Id = 3, FirstName = "Юрий", LastName = "Кижиков", MiddleName = "Витальевич" }
+            };
+        }
+    }
+}
